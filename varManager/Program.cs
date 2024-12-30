47f68a61717ecc683899dcd7dea5e9aeba916100
unknown <gfp1987@gmail.com>
2024-12-31 00:15:25 +0800

    Fix build issue. Default to hide|fav view.

--- varManager/Program.cs
+++ varManager/Program.cs
@@ -13,7 +13,7 @@ namespace varManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormScenes());
         }
     }
 }
--- varManager/Properties/Settings.Designer.cs
+++ varManager/Properties/Settings.Designer.cs
@@ -50,7 +50,7 @@ namespace varManager.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\varManager.mdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\varManager.mdb")]
         public string varManagerConnectionString {
             get {
                 return ((string)(this["varManagerConnectionString"]));
